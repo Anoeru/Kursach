8b8f84633781628c61988e3152423b8057de33a1
anoeru <47786730+Anoeru@users.noreply.github.com>
2019-05-18 21:58:54 +0300

    Update MyForm1.h

--- MyForm1.h
+++ MyForm1.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <vector>
 namespace Project4 {
 
 	using namespace System;
@@ -16,7 +17,7 @@ namespace Project4 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Сводка для MyForm1
+	/// РЎРІРѕРґРєР° РґР»СЏ MyForm1
 	/// </summary>
 	public ref class MyForm1 : public System::Windows::Forms::Form
 	{
@@ -25,13 +26,13 @@ namespace Project4 {
 		{
 			InitializeComponent();
 			//
-			//TODO: добавьте код конструктора
+			//TODO: РґРѕР±Р°РІСЊС‚Рµ РєРѕРґ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Освободить все используемые ресурсы.
+		/// РћСЃРІРѕР±РѕРґРёС‚СЊ РІСЃРµ РёСЃРїРѕР»СЊР·СѓРµРјС‹Рµ СЂРµСЃСѓСЂСЃС‹.
 		/// </summary>
 		~MyForm1()
 		{
@@ -54,26 +55,29 @@ namespace Project4 {
 	private: System::Windows::Forms::NumericUpDown^  numericUpDown2;
 	private: System::Windows::Forms::Label^  label4;
 	private: System::Windows::Forms::Label^  label5;
-	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	private: System::Windows::Forms::MenuStrip^  menuStrip1;
 	private: System::Windows::Forms::ToolStripMenuItem^  infoToolStripMenuItem;
 	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Button^  button4;
+
 	protected:
 
 	private:
 		/// <summary>
-		/// Обязательная переменная конструктора.
+		/// РћР±СЏР·Р°С‚РµР»СЊРЅР°СЏ РїРµСЂРµРјРµРЅРЅР°СЏ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
+		/// РўСЂРµР±СѓРµРјС‹Р№ РјРµС‚РѕРґ РґР»СЏ РїРѕРґРґРµСЂР¶РєРё РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР° вЂ” РЅРµ РёР·РјРµРЅСЏР№С‚Рµ 
+		/// СЃРѕРґРµСЂР¶РёРјРѕРµ СЌС‚РѕРіРѕ РјРµС‚РѕРґР° СЃ РїРѕРјРѕС‰СЊСЋ СЂРµРґР°РєС‚РѕСЂР° РєРѕРґР°.
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
@@ -84,13 +88,14 @@ namespace Project4 {
 			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->infoToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -119,7 +124,6 @@ namespace Project4 {
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(186, 20);
 			this->textBox1->TabIndex = 2;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox1_TextChanged);
 			// 
 			// button2
 			// 
@@ -137,7 +141,6 @@ namespace Project4 {
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(186, 20);
 			this->textBox3->TabIndex = 5;
-			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox3_TextChanged);
 			// 
 			// label3
 			// 
@@ -181,15 +184,6 @@ namespace Project4 {
 			this->label5->TabIndex = 13;
 			this->label5->Text = L"func";
 			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(-258, -132);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(619, 441);
-			this->pictureBox1->TabIndex = 14;
-			this->pictureBox1->TabStop = false;
-			// 
 			// menuStrip1
 			// 
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->infoToolStripMenuItem });
@@ -216,11 +210,38 @@ namespace Project4 {
 			this->button3->UseVisualStyleBackColor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &MyForm1::button3_Click);
 			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(125, 124);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(123, 20);
+			this->textBox2->TabIndex = 17;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(125, 149);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(123, 20);
+			this->textBox4->TabIndex = 18;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(253, 38);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(19, 18);
+			this->button4->TabIndex = 19;
+			this->button4->Text = L"R";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm1::button4_Click);
+			// 
 			// MyForm1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
@@ -233,14 +254,12 @@ namespace Project4 {
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->menuStrip1);
-			this->Controls->Add(this->pictureBox1);
 			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm1";
 			this->Text = L"Change";
 			this->Load += gcnew System::EventHandler(this, &MyForm1::MyForm1_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
@@ -280,10 +299,9 @@ namespace Project4 {
 	}
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
 		int found = 0;  
-		int y;
-		char m;
-		int sch = 0;
+		int sch = 1;
 		Basic st;
+		std::string g;
 		int n = System::Convert::ToInt32(textBox1->Text);
 		std::fstream File;
 		File.open("req.txt", std::ios::in | std::ios::out);     
@@ -291,16 +309,12 @@ namespace Project4 {
 			MessageBox::Show("File could not be open !! Press any Key...");
 		return;
 		}
-		while (!File.eof()) {
-			st.Load();
-			sch++;
-			if (st.getNum() == n) {
-				//textBox2->Text = " ";
-				//textBox3->Text = " ";
-				//numericUpDown1->Text = "0";
-				//numericUpDown2->Text = "0";
-				n = System::Convert::ToDouble(numericUpDown1->Text);
-				st.setState(n);
+		do {
+			st.Load(sch);
+			std::ofstream out;
+			out.open("req1.txt", std::ios::app);
+			out << st.getNum() << "\n";
+			if (st.getNum() == n) {			
 				std::string s;
 				System::String^ managed = textBox3->Text;
 				s = msclr::interop::marshal_as<std::string>(managed);
@@ -308,17 +322,22 @@ namespace Project4 {
 				{
 					st.setreq(s);
 				}
+				n = System::Convert::ToDouble(numericUpDown1->Text);
+				st.setState(n);
 				n = System::Convert::ToDouble(numericUpDown2->Text);
 				st.setF(n);				
-				//int pos = (-1) * sizeof(st);
-				//File.seekp(pos, std::ios::cur);
 				st.Disk();
-				//File.write((char *)&st, sizeof(Basic));
 				MessageBox::Show("Record Updated");
-				found = 1;
-				//remove_line("req.txt", sch);				
+				found = remove_line("req.txt", sch);
+				break;
 			}
-		}
+			else
+			{
+				std::getline(File, g);
+				g.clear();
+			}
+			sch++;
+		} while (!File.eof());
 		File.close();
 		if (found == 0)
 			MessageBox::Show("\n\n Record Not Found ");
@@ -332,8 +351,11 @@ private: System::Void infoToolStripMenuItem_Click(System::Object^  sender, Syste
 }
 private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
 	Basic st;
+	bool l;
+	std::string g;
+	int sch = 1;
 	std::ifstream inFile;
-	System::String^ managed = "0";
+	System::String^ managed = "";
 	int n = System::Convert::ToInt32(textBox1->Text);
 	inFile.open("req.txt", std::ios::beg);    
 	if (!inFile) {
@@ -343,29 +365,46 @@ private: System::Void button3_Click(System::Object^  sender, System::EventArgs^ 
 	int flag = 0;    
 	do
 	{
-		st.Load();//textBox1->Text = managed;
-	if (st.getNum() == n) {		
+		st.Load(sch);
+		sch++;
+		std::ofstream out;
+		out.open("req1.txt", std::ios::app);
+		out << st.getNum()<<"\n";
+		if (st.getNum() == n) {		
 		std::string s;
-		msclr::interop::marshal_as<std::string>(managed) = s;
-		textBox1->Text = managed;
-		for (size_t idx = 0; idx < s.length(); idx++)
-		{
-			s[idx] = st.getreq(idx);
-		}
-		msclr::interop::marshal_as<std::string>(managed) = s;
+		s=st.getreq();
+		managed = gcnew System::String(s.c_str());
 		textBox3->Text = managed;
-
-	flag = 1;
-	}
+		l = st.getFunc();
+		if (l == 0)
+			textBox4->Text = "Functional";
+		else
+			textBox4->Text = "Not Functional";
+		l = st.getState();
+		if (l == 0)
+			textBox2->Text = "Not done";
+		else
+			textBox2->Text = "Done";
+		flag = 1;
+		break;
+		}
+		else
+		{
+			std::getline(inFile, g);
+			g.clear();
+		}
 	} while (!inFile.eof());
 	inFile.close();     
 	if (flag == 0)         
-		MessageBox::Show("\n\nrecord not exist");
-}
-private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
-}
-		 
-private: System::Void textBox3_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+		MessageBox::Show("\n\nRecord doesn't exist");
+}	 
+private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+	textBox1->Text = "";
+	textBox2->Text = "";
+	textBox3->Text = "";
+	textBox4->Text = "";
+	numericUpDown1->Text = "0";
+	numericUpDown2->Text = "0";
 }
 };
 }
