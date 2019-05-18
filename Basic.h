eede6d19d8ea06d6f1d5cc5ab30afaf0ca2c7458
anoeru <47786730+Anoeru@users.noreply.github.com>
2019-05-18 21:55:31 +0300

    Update Basic.h

--- Basic.h
+++ Basic.h
@@ -26,4 +26,31 @@ public:
 	void Disk();
 	char getreq(int);
 	~Basic() {}
+};#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+class Basic
+{
+protected:
+	int num;
+	std::string req;
+	bool func;
+	bool state;
+public:
+	Basic();
+	int getNum();
+	bool getState();
+	bool getFunc();
+	void setnum(int);
+	void setreq(std::string&);
+	void Load(int);
+	void setF(bool);
+	void setState(bool);
+	void Disk();
+	std::string getreq();
+	~Basic() {}
 };
